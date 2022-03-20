d49fa56acba409bc48b6161de9d59af4a885d19d
AlbertAK <albert.ahimbisibwe@bposeats.com>
2022-03-20 22:20:47 +0300

    Added config and utilities

---
+++ src/test/java/com/leadpops/pageObjects/TestData.java
@@ -0,0 +1,20 @@
+package com.leadpops.pageObjects;
+
+public class TestData {
+
+	String firstName = "Albert";
+	String lastName = "Ahimbisibwe";
+	String middleName = "Testing";
+	String emailAddress = "albert@gmail.com";
+	String contactNumber = "0701123456";
+	String homeNumber = "0780123456";
+	String residenceAddress = "Mbarara";
+	String unitNumber = "10";
+	String city = "Mbarara";
+	String zipCode = "10";
+	String currentEmployer = "BPO";
+	String yearsAtEmployer = "2";
+	String employerPhoneNumber = "0700221144";
+	String employerAddress = "Mbarara, Uganda";
+	String position = "Manager";
+}
